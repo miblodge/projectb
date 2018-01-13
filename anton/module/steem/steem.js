67c5250ed081cff0690008e3afc17833480ab0fd
antonchanning <antonchanning@gmail.com>
2018-01-13 20:04:20 +0000

    Added dateformat support

--- anton/module/steem/steem.js
+++ anton/module/steem/steem.js
@@ -15,7 +15,7 @@ function steem_menuitem(args) {
 	var args_json = JSON.stringify(args);
 	// get json object ready for injecting into html href...
 	args_json = args_json.replace(/"/g,'&quot;');
-	console.log(args_json);
+	//console.log(args_json);
 	return "javascript:steem_load('"+args_json+"');";
 }
 
@@ -24,15 +24,15 @@ function steem_display(content) {
 }
 function steem_load(args) {
 	steem_args = args;
-	console.log("Inside steem_load.");
-console.log('args: '+args);
+	//console.log("Inside steem_load.");
+//console.log('args: '+args);
 	args = JSON.parse(args.replace(/&quot;/g,'"'));
-console.log('args JSON parsed: '+args);
-	console.log(args.show);
+//console.log('args JSON parsed: '+args);
+	//console.log(args.show);
 
 	switch(args.show) {
 		case "profile":
-			console.log("Get profile of: "+args.user);
+			//console.log("Get profile of: "+args.user);
 			getSteemProfile(args.user);
 			break;
 		case "posts":
@@ -44,23 +44,28 @@ console.log('args JSON parsed: '+args);
 			break;
 		case "post":
 			getSteemPost(args.user,args.postid);
-			console.log("post: "+args.user+" "+args.postid);
+			//console.log("post: "+args.user+" "+args.postid);
 			break;
 	}
 }
+function steemPostsDisplayThumbnail(postid){
+	console.log("steemPostsDisplayThumbnail: "+postid);
+	$('#'+postid+' img.steemposts_thumbnail').css("display",'inline-block');
+	$('#'+postid+' span.steem_posts_preview').css("width",'70%');
 
+}
 function displaySteemPosts(err, posts) {
 	var template = steem_posts_template;
 	// Save posts in global variable
-	console.log(posts);
+	//console.log(posts);
 	steem_posts = posts;
 
-console.log("steem_username: "+steem_username);
+//console.log("steem_username: "+steem_username);
 
 	var args = steem_args;
-console.log("displaySteemPosts args: "+args);
+//console.log("displaySteemPosts args: "+args);
 	var content = '', display_count = steem_posts_displayed.length;
-	var json_metadata, post_tags, post_obj, body, show_post, last_date, last_permlink, loop_end = false;
+	var json_metadata, post_tags, post_obj, body, show_post, last_date, last_permlink, created_date, display_date, loop_end = false;
 
 	var converter = new showdown.Converter();
 
@@ -83,12 +88,13 @@ console.log("displaySteemPosts args: "+args);
 		//console.log("Post "+i+": "+JSON.stringify(post_obj));
 		//console.log("metadata "+i+": "+JSON.stringify(json_metadata));
 
-		console.log("permlink "+i+": "+JSON.stringify(post_obj.permlink));
-		console.log("created "+i+": "+JSON.stringify(post_obj.created));
+		//console.log("permlink "+i+": "+JSON.stringify(post_obj.permlink));
+		//console.log("created "+i+": "+JSON.stringify(post_obj.created));
 
 		if(steem_posts_displayed.indexOf(post_obj.permlink) !== -1) {
 			//loop_end = true;
 			show_post = false;
+	console.log("Duplicate. Ignore: "+last_permlink);
 		} else {
 			last_permlink = post_obj.permlink;
 	console.log("Last permlink: "+last_permlink);
@@ -101,11 +107,15 @@ console.log("displaySteemPosts args: "+args);
 			//template+="json_metadata "+i+": "+JSON.stringify(json_metadata);
 
 			// Insert post values for this post
-			template = template.replace('{steem_posts_tag}',post_obj.category);
-			template = template.replace('{steem_posts_title}',post_obj.title);
-			template = template.replace('{steem_posts_permlink}',post_obj.permlink);
-			template = template.replace('{steem_posts_img}',json_metadata.image);
-			template = template.replace('{steem_posts_author}',post_obj.author);
+			template = template.replace(/{steem_posts_tag}/g,post_obj.category);
+			template = template.replace(/{steem_posts_title}/g,post_obj.title);
+			template = template.replace(/{steem_posts_permlink}/g,post_obj.permlink);
+			template = template.replace(/{steem_posts_img}/g,json_metadata.image);
+			template = template.replace(/{steem_posts_author}/g,post_obj.author);
+
+			created_date=new Date(post_obj.created);
+			display_date = created_date.toLocaleDateString(config.dateformat.locale, config.dateformat.options);
+			template = template.replace(/{steem_posts_date}/g,display_date);
 
 			// Full body
 			body = converter.makeHtml(post_obj.body);
@@ -121,33 +131,33 @@ console.log("displaySteemPosts args: "+args);
 			//Reset template
 			var template = steem_posts_template;
 		}
+		//console.log("posts: "+postsLength+"/"+steem_posts_count);
+		if(postsLength<steem_posts_count) loop_end=true; // Got less than we wanted, display no more.
 	}
 	if(display_count < steem_posts_count && !loop_end) {
-console.log("displaySteemPosts b4 getSteemPosts");
-console.log("username: "+steem_username);
-console.log("steem_tags: "+steem_tags);
-console.log("steem_posts_count: "+steem_posts_count);
-console.log("last_permlink: "+last_permlink);
+//console.log("username: "+steem_username);
+//console.log("steem_posts_count: "+steem_posts_count);
+//console.log("last_permlink: "+last_permlink);
 		getSteemPosts(steem_username,steem_tags,steem_posts_count,last_permlink);
 	}
 	if(loop_end) console.log('Blocked infinite loop. Quitting...');
 	if(display_count >= steem_posts_count) {
-		console.log('Reached post limit. Quitting...');
-		console.log(steem_posts_displayed);
+		//console.log('Reached post limit. Quitting...');
+		//console.log(steem_posts_displayed);
 	}
 	// Append content to contentArea
 	$('#contentArea').append(content);
 }
 function getSteemPosts(usernames,tags,count,lastPermlink) {
-console.log('getSteemPosts usernames: '+usernames+' tags: '+tags+' count: '+count+' lastPermlink: '+lastPermlink);
+//console.log('getSteemPosts usernames: '+usernames+' tags: '+tags+' count: '+count+' lastPermlink: '+lastPermlink);
 	if (typeof count === "undefined") count=10; // Default to 10 if no count specified.
 	if (typeof lastPermlink === "undefined") lastPermlink=''; // Default to 10 if no count specified.
 
 	steem_tags = tags;
 	steem_posts_count = count;
 	steem_username = usernames;
-console.log('usernames: '+usernames+' tags: '+tags+' count: '+count+' lastPermlink: '+lastPermlink);
-
+	//console.log('usernames: '+usernames+' tags: '+tags+' count: '+count+' lastPermlink: '+lastPermlink);
+	steem.api.setOptions({ url: 'https://api.steemit.com' });
 	steem.api.getDiscussionsByAuthorBeforeDate(usernames, lastPermlink, '2100-01-01T00:00:00', count,	function(err, result){displaySteemPosts(err, result)});
 	//displaySteemPosts(['AAA']);
 }
@@ -158,21 +168,29 @@ function displaySteemPost(err, post) {
 
 	var template = steem_post_template;
 
+
+	// Insert post values for this post
+	template = template.replace(/{steem_post_category}/g,steem_post.category);
+	template = template.replace(/{steem_post_title}/g,steem_post.title);
+	template = template.replace(/{steem_post_permlink}/g,steem_post.permlink);
+	template = template.replace(/{steem_post_author}/g,steem_post.author);
+
 	// Display template
 	$('#contentArea').html(template);
 
-		console.log(steem_post.body);
+		//console.log(steem_post.body);
 // Convert markdown into html
 	var converter = new showdown.Converter();
 	var body_html = converter.makeHtml(steem_post.body);
 
-	console.log(body_html);
+	//console.log(body_html);
 	// Then add post values
 	$("#steem-post-content").html(body_html);
 	$("#steem-post-title").html(steem_post.title);
 }
 function getSteemPost(username,postid) {
 	//
+	steem.api.setOptions({ url: 'https://api.steemit.com' });
 	steem.api.getContent(username, postid,	function(err, result){displaySteemPost(err, result)});
 }
 
@@ -201,6 +219,7 @@ template = template.replace('{steem_profile_location}',metadata.profile.location
 	console.log(metadata.profile);
 	console.log("profile image: "+metadata.profile.profile_image);
 	$("#profile-banner").css("background","url("+metadata.profile.cover_image+") no-repeat");
+	$("#profile-banner").css("background-position","center");
 	$("#profile-image").html('<img src="'+metadata.profile.profile_image+'">');
 	$("#profile-username").html('@'+steem_profile.name);
 	$("#profile-name").html(metadata.profile.name);
@@ -212,6 +231,7 @@ template = template.replace('{steem_profile_location}',metadata.profile.location
 }
 function getSteemProfile(username) {
 	//
+	steem.api.setOptions({ url: 'https://api.steemit.com' });
 	steem.api.getAccounts([username],	function(err, profile){displaySteemProfile(err, profile)});
 }
 
